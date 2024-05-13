c4387f567ceb2ccf0e38bb0d7e18aea5d5d78099
mirko2de <mirkoacosta55@gmail.com>
2024-05-13 11:38:10 -0300

    se corrigio el archivo FizzBuzz.js

--- Desktop/control_de_flujo/assets/FizzBuzz.js
+++ Desktop/control_de_flujo/assets/FizzBuzz.js
@@ -2,13 +2,13 @@
 
 for (let i = 1; i <= 15; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz");
+        console.log(i,"FizzBuzz");
     }
     else if (i % 3 === 0) {
-        console.log("Fizz");
+        console.log(i,"Fizz");
     }
     else if (i % 5 === 0) {
-        console.log("Buzz");
+        console.log(i,"Buzz");
     }    else {
         console.log(i);
     }
